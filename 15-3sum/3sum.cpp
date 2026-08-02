5ff7e046e2fd52e4bacb1a44741eb0977705377d
Harsh Vardhan Singh Yadav <141325232+Iampanchi@users.noreply.github.com>
2026-08-03 01:44:18 +0530

    Time: 63 ms (16.85%) | Memory: 29.1 MB (47.38%) - LeetSync

--- 15-3sum/3sum.cpp
+++ 15-3sum/3sum.cpp
@@ -2,15 +2,22 @@ class Solution {
 public:
     vector<vector<int>> threeSum(vector<int>& nums) {
         sort(nums.begin(),nums.end());
-        set<vector<int>> v;
-        // if(nums.size()>=3 && nums[0]==0 && nums[1]==0 && nums[2]==0){
-        //     v.insert({0,0,0});
-        // }
+        vector<vector<int>> v;
+
         for(int i = 0 ;i<nums.size()-2;i++){
+            if(i!=0 && nums[i]==nums[i-1]){
+                continue;
+            }
             int sum=-1;
             int j=i+1,k=nums.size()-1;
-            
+            long long lstj=1e8,lstk=1e8;
             while(sum!=0 && j<k){
+                if(nums[j]==lstj && nums[k]==lstk){
+                    j++;
+                    k--;
+                    continue;
+
+                }
                 sum=nums[i]+nums[j]+nums[k];
                 if(sum>0){
                     k--;
@@ -19,18 +26,20 @@ public:
                     j++;
                 }
                 else{
-                 v.insert({nums[i],nums[j],nums[k]});
+                 v.push_back({nums[i],nums[j],nums[k]});
+                 lstj=nums[j];
+                 lstk=nums[k];
                  j++;
                  k--;
+                 
+                 
                  sum=-1;
                 }
             }
-            if(sum==0 && j<k){
-                 v.insert({nums[i],nums[j],nums[k]});
-            }
+            
             
         }
-        vector<vector<int>> p(v.begin(),v.end());
-        return p;
+        
+        return v;
     }
 };
