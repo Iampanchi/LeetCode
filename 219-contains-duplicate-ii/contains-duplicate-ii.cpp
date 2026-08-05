edd7d66d1af3a0cc4ac84ea471a6fe88e18cee80
Harsh Vardhan Singh Yadav <141325232+Iampanchi@users.noreply.github.com>
2026-08-05 17:38:59 +0530

    Time: 329 ms (5.49%) | Memory: 177.5 MB (5.17%) - LeetSync

---
+++ 219-contains-duplicate-ii/contains-duplicate-ii.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    bool containsNearbyDuplicate(vector<int>& nums, int k) {
+        int i =0;
+        map<int,vector<int>> mp;
+        for(int i = 0 ;i<nums.size();i++){
+            mp[nums[i]].push_back(i);
+        }
+        for(auto it = mp.begin();it!=mp.end();it++){
+            int lst = it->second[0];
+            int cur = it->second[0];
+            for(int i=1;i<it->second.size();i++){
+                cur=it->second[i];
+                if(cur-lst<=k){
+                    return true;
+                }
+                lst=cur;
+            }
+        }
+        return false;
+    }
+};
