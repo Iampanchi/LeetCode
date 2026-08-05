ae85b1529785714a8d4676dd155d98d7c6ee7fad
Harsh Vardhan Singh Yadav <141325232+Iampanchi@users.noreply.github.com>
2026-08-05 17:44:31 +0530

    Time: 199 ms (8.73%) | Memory: 161 MB (5.17%) - LeetSync

--- 219-contains-duplicate-ii/contains-duplicate-ii.cpp
+++ 219-contains-duplicate-ii/contains-duplicate-ii.cpp
@@ -1,22 +1,43 @@
+// class Solution {
+// public:
+//     bool containsNearbyDuplicate(vector<int>& nums, int k) {
+//         int i =0;
+//         unordered_map<int,vector<int>> mp;
+//         for(int i = 0 ;i<nums.size();i++){
+//             mp[nums[i]].push_back(i);
+//         }
+//         for(auto it = mp.begin();it!=mp.end();it++){
+//             int lst = it->second[0];
+//             int cur = it->second[0];
+//             for(int i=1;i<it->second.size();i++){
+//                 cur=it->second[i];
+//                 if(cur-lst<=k){
+//                     return true;
+//                 }
+//                 lst=cur;
+//             }
+//         }
+//         return false;
+//     }
+// };
+
 class Solution {
 public:
     bool containsNearbyDuplicate(vector<int>& nums, int k) {
-        int i =0;
-        unordered_map<int,vector<int>> mp;
+        unordered_map<int,int> mp,fnd;
         for(int i = 0 ;i<nums.size();i++){
-            mp[nums[i]].push_back(i);
-        }
-        for(auto it = mp.begin();it!=mp.end();it++){
-            int lst = it->second[0];
-            int cur = it->second[0];
-            for(int i=1;i<it->second.size();i++){
-                cur=it->second[i];
-                if(cur-lst<=k){
+            if(!fnd[nums[i]]){
+                fnd[nums[i]]=1;
+                
+            }
+            else{
+                if(i-mp[nums[i]]<=k){
                     return true;
                 }
-                lst=cur;
             }
+            mp[nums[i]]=i;
         }
         return false;
+        
     }
 };
