e5ab965fcd5cf2609422e48c14a29fa3d31eebae
Harsh Vardhan Singh Yadav <141325232+Iampanchi@users.noreply.github.com>
2026-09-04 21:34:58 +0530

    Time: 3 ms (67.71%) | Memory: 11.8 MB (99.36%) - LeetSync

---
+++ 108-convert-sorted-array-to-binary-search-tree/convert-sorted-array-to-binary-search-tree.c
@@ -0,0 +1,64 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     struct TreeNode *left;
+ *     struct TreeNode *right;
+ * };
+ */ 
+int* arr;
+ struct TreeNode* createNode(){
+    struct TreeNode* node = malloc(sizeof(struct TreeNode));
+    node->left=NULL;
+    node->right=NULL;
+    return node;
+}
+ void hierarchy( int lo , int hi ,struct TreeNode* root )
+{   if(lo>hi){
+    return;
+    }
+    int mid = lo + (hi-lo)/2;
+    // if(dir==0){
+        root->val=arr[mid];
+        if(lo==hi){
+            return;
+        }
+        if(lo<mid){
+        struct TreeNode *lft=createNode();
+        root->left=lft;
+        hierarchy(lo,mid-1,root->left);
+    }
+    if(hi>mid){
+        struct TreeNode *rght=createNode();
+        root->right=rght;
+        hierarchy(mid+1,hi,rght);
+    }
+}
+struct TreeNode* sortedArrayToBST(int* nums, int numsSize) {
+    arr=malloc((numsSize+1)*sizeof(int));
+    for(int i = 0 ;i<numsSize;i++){
+        arr[i+1]=nums[i];
+    }
+    int lo = 1 , hi = numsSize;
+
+    int mid = lo + (hi-lo)/2;
+    struct TreeNode* root=createNode();
+    root->val=arr[mid];
+    if(lo<mid){
+        struct TreeNode *lft=createNode();
+        root->left=lft;
+        hierarchy(lo,mid-1,root->left);
+    }
+    if(hi>mid){
+        struct TreeNode *rght=createNode();
+        root->right=rght;
+        hierarchy(mid+1,hi,rght);
+    }
+    
+    
+    return root;
+    
+
+    
+
+}
