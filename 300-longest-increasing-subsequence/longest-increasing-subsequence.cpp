06bb03b48e9d2f08f4c26b14383b4d437566f638
Harsh Vardhan Singh Yadav <141325232+Iampanchi@users.noreply.github.com>
2026-09-07 01:14:13 +0530

    Time: 3 ms (79.19%) | Memory: 13.9 MB (94.74%) - LeetSync

---
+++ 300-longest-increasing-subsequence/longest-increasing-subsequence.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int lengthOfLIS(vector<int>& nums) {
+        int n=nums.size();
+        vector<int> tails;
+        tails.push_back(nums[0]);
+        for(int i = 1 ; i<n;i++){
+            if(tails.back()<nums[i]){
+                tails.push_back(nums[i]);
+            }
+            else{
+                int p = lower_bound(tails.begin(),tails.end(),nums[i])-tails.begin();
+                tails[p]=nums[i];
+            }
+        }
+        return tails.size();
+    }
+};
